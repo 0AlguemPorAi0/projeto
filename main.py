041b1847224a72f75198ee7446a34477e4843941
felipebueno-hub <xwagkbievktavwfjyn@enotj.com>
2025-09-20 19:33:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def root():
 
 @app.get("/fila")
 async def senha():
-    return {"senha": True, "num_aleatorio": random.randint(0, 5000)}
+    return {"senha": True, "num_aleatorio": random.randint(0, 1000)}
 
 @app.post("/carros/", response_model=Carro)
 async def criar_carro(carro: Carro):
